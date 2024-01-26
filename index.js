3e3a126981820c92eb7bdd4ab08ed1e6b832d455
RyannKim327 <rksesgundo123@gmail.com>
2024-01-26 21:14:10 +0800

    01-26-24 21:14

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const url = "https://projectcloudbased.vercel.app/convert"
+let converting = true
 
 document.getElementById("url").onchange = () => {
 	const urlData = document.getElementById("url").value
@@ -17,32 +18,36 @@ document.getElementById("url").onchange = () => {
 }
 
 function requestDownload(videoURL){
-	fetch(url, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json"
-		},
-		body: JSON.stringify({
-			url: encodeURI(videoURL)
+	if(converting){
+		fetch(url, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				url: encodeURI(videoURL)
+			})
+		}).then(response => {
+			return response.json()
+		}).then(response => {
+			const lists = document.getElementById("links")
+			document.getElementById("title").textContent = `${response.title}`
+			document.getElementById("source").textContent = `Source: ${response.source}`
+			document.getElementById("duration").textContent = `${response.duration}`
+			const data = response.medias
+			for(let d in data){
+				const a = document.createElement("a")
+				a.download = ""
+				a.target = "_blank"
+				a.href = data[d].url
+				a.textContent = `Download ${data[d].extension} ${data[d].quality} (${data[d].formattedSize})`
+				window.URL.revokeObjectURL(data[d].url)
+				lists.appendChild(a)
+			}
+			converting = false
+		}).catch(error => {
+			alert(error)
+			converting = false
 		})
-	}).then(response => {
-		return response.json()
-	}).then(response => {
-		const lists = document.getElementById("links")
-		document.getElementById("title").textContent = `${response.title}`
-		document.getElementById("source").textContent = `Source: ${response.source}`
-		document.getElementById("duration").textContent = `${response.duration}`
-		const data = response.medias
-		for(let d in data){
-			const a = document.createElement("a")
-			a.download = ""
-			a.target = "_blank"
-			a.href = data[d].url
-			a.textContent = `Download ${data[d].extension} ${data[d].quality} (${data[d].formattedSize})`
-			window.URL.revokeObjectURL(data[d].url)
-			lists.appendChild(a)
-		}
-	}).catch(error => {
-		alert(error)
-	})
+	}
 }
